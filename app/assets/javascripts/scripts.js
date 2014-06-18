14c6b332b4ce9355255e8bd689fcf943235077c6
daramao <dara.mao@gmail.com>
2014-06-18 14:46:22 -0400

    fixed margin

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -3,74 +3,80 @@
 // for breaking the database weather strings into number arrays.
 // if NaN, replaces with -1 for easier data manipulation with d3.
 function splitToNumArray(string){
-    var results_array = [];
-    var temp_array = string.split(",");
-    temp_array.forEach(function(num) {
-        results_array.push(parseFloat(num));
-    });
-    results_array.forEach(function(x,i) {
-        if (isNaN(x)) {
-            results_array[i] = -1;
-        }
-    })
-    return results_array;
+  var results_array = [];
+  var temp_array = string.split(",");
+  temp_array.forEach(function(num) {
+    results_array.push(parseFloat(num));
+  });
+  results_array.forEach(function(x,i) {
+    if (isNaN(x)) {
+      results_array[i] = -1;
+    }
+  })
+  return results_array;
 }
 
 
 function maxOfArray(numArray){
-    return Math.max.apply(null, numArray);
+  return Math.max.apply(null, numArray);
 }
 
 function minOfArray(numArray){
-    return Math.min.apply(null, numArray);
+  return Math.min.apply(null, numArray);
 }
 
 function combineMinMeanMax(minArray, meanArray, maxArray){
-    var dataset = [];
-    for (i=0; i < minArray.length; i++) {
-        var combinedArray = [];
-        combinedArray[0] = minArray[i];
-        combinedArray[1] = meanArray[i];
-        combinedArray[2] = maxArray[i];
-        dataset.push(combinedArray);
-    }
-    return dataset;
+  var dataset = [];
+  for (i=0; i < minArray.length; i++) {
+    var combinedArray = [];
+    combinedArray[0] = minArray[i];
+    combinedArray[1] = meanArray[i];
+    combinedArray[2] = maxArray[i];
+    dataset.push(combinedArray);
+  }
+  return dataset;
 }
 
 
 // calendar functionality
 function sortCalendarData(objects, team_id) {
-    var teamName = team_id.split("_").join(" ");
-    var gameObjects = {};
-    objects.forEach(function(game){
-        if ( game.home_team == teamName ){
-            var dateArray = game.date.split("-");
-            var dateString = (dateArray[1]+"-"+dateArray[2]+"-"+dateArray[0]);
-            gameObjects[dateString] = ("<a class='calendar_entry' href=\"baseballs/"+game.id+"\"><p>" + game.away_team + " @<br> " + game.home_team + "</p><p class='game_time'>" + game.time + "</p></a>");
-        }
-    });
-    return gameObjects;
+  var teamName = team_id.split("_").join(" ");
+  var gameObjects = {};
+  objects.forEach(function(game){
+    if ( game.home_team == teamName ){
+      var dateArray = game.date.split("-");
+      var dateString = (dateArray[1]+"-"+dateArray[2]+"-"+dateArray[0]);
+      gameObjects[dateString] = ("<a class='calendar_entry' href=\"baseballs/"+game.id+"\"><p>" + game.away_team + " @<br> " + game.home_team + "</p><p class='game_time'>" + game.time + "</p></a>");
+    }
+  });
+  return gameObjects;
 }
 
 $(document).foundation({
-    dropdown: {
-        active_class: 'open'
-    }
+  dropdown: {
+    active_class: 'open'
+  }
 });
 
 
 function makeBoxes(dataset) {
 
+  var margin = {top: 50, right: 400, bottom: 20, left: 400},
+  // padding = 30,
+  // width = 400,
+  // height = 200,
+  width = 1350 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom,
 
-  var height = 300,
-      width = 800,
-      barPad = 60,
-      gWidth = width /dataset.length,
-      barWidth = gWidth - barPad,
-      dataObjects = [];
+  // var height = 300,
+  // width = 800,
+  barPad = 60,
+  gWidth = width /dataset.length,
+  barWidth = gWidth - barPad,
+  dataObjects = [];
 
   var absMin = Infinity,
-      absMax = -Infinity;
+  absMax = -Infinity;
 
   dataset.forEach(function(d){
     if (d[0] < absMin) {
@@ -83,52 +89,52 @@ function makeBoxes(dataset) {
 
   var pixPerTemp = height / (absMax - absMin);
 
-    dataset.forEach(function(d,i){
-  var min = d[0],
-      mean = d[1],
-      max = d[2],
-      rects = [],
-      minRect = {},
-      maxRect = {};
+  dataset.forEach(function(d,i){
+    var min = d[0],
+    mean = d[1],
+    max = d[2],
+    rects = [],
+    minRect = {},
+    maxRect = {};
 
-  minRect['rx'] = i * gWidth
-  minRect['ry'] = pixPerTemp * (absMax - mean)
-  minRect['rheight'] = pixPerTemp * (mean-min)
-  minRect['rwidth'] =  barWidth
-  minRect['color'] = 'blue';
+    minRect['rx'] = i * gWidth
+    minRect['ry'] = pixPerTemp * (absMax - mean)
+    minRect['rheight'] = pixPerTemp * (mean-min)
+    minRect['rwidth'] =  barWidth
+    minRect['color'] = 'blue';
 
-  maxRect['rx'] = i * gWidth
-  maxRect['ry'] = pixPerTemp * (absMax - max)
-  maxRect['rheight'] = pixPerTemp * (max-mean);
-  maxRect['rwidth'] = barWidth;
-  maxRect['color'] = 'red';
-  rects.push(minRect, maxRect);
+    maxRect['rx'] = i * gWidth
+    maxRect['ry'] = pixPerTemp * (absMax - max)
+    maxRect['rheight'] = pixPerTemp * (max-mean);
+    maxRect['rwidth'] = barWidth;
+    maxRect['color'] = 'red';
+    rects.push(minRect, maxRect);
 
-  dataObjects.push(rects);
+    dataObjects.push(rects);
   })
 
 
-    var svg = d3.select("body").append("svg")
-      .attr("class", "weather-svg")
+  var svg = d3.select("body").append("svg")
+  .attr("class", "weather-svg")
 
-    var g = svg.selectAll("g")
-        .data(dataObjects)
-        .enter()
-        .append("g")
-        .attr("class", "year-g")
-        .attr("id", function(d,i) {
-           return "g-" + i;
-         })
+  var g = svg.selectAll("g")
+  .data(dataObjects)
+  .enter()
+  .append("g")
+  .attr("class", "year-g")
+  .attr("id", function(d,i) {
+    return "g-" + i;
+  })
 
-    g.selectAll("rect")
-        .data(function(d) {return d; })
-        .enter()
-        .append("rect")
-        .attr("x", function(d) { return d.rx})
-        .attr("y", function(d) { return d.ry})
-        .attr("height", function(d) { return d.rheight})
-        .attr("width", function(d) {return d.rwidth})
-    .style("fill", function(d) { return d.color })
+  g.selectAll("rect")
+  .data(function(d) {return d; })
+  .enter()
+  .append("rect")
+  .attr("x", function(d) { return d.rx})
+  .attr("y", function(d) { return d.ry})
+  .attr("height", function(d) { return d.rheight})
+  .attr("width", function(d) {return d.rwidth})
+  .style("fill", function(d) { return d.color })
 }
 
 
@@ -136,57 +142,59 @@ function makeBoxes(dataset) {
 
 function make_axes(){
 
-    var margin = {top: 20, right: 0, bottom: 20, left: 0},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  var margin = {top: 20, right: 0, bottom: 20, left: 0},
+  width = 1300 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-    var formatNumber = d3.format(".1f");
+  var formatNumber = d3.format(".1f");
 
-    var y = d3.scale.linear()
-        .domain([0, 100e6])
-        .range([height, 0]);
+  var y = d3.scale.linear()
+  .domain([0, 100e6])
+  .range([height, 0]);
 
-    var x = d3.time.scale()
-        .domain([new Date(2007, 7, 1), new Date(2013, 7, 1)])
-        .range([0, width]);
+  var x = d3.time.scale()
+  .domain([new Date(2007, 7, 1), new Date(2013, 7, 1)])
+  .range([0, width]);
 
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .ticks(d3.time.years)
-        .orient("bottom");
+  var xAxis = d3.svg.axis()
+  .scale(x)
+  .ticks(d3.time.years)
+  .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .tickSize(width)
-        .tickFormat(formatTemperature)
-        .orient("right");
+  var yAxis = d3.svg.axis()
+  .scale(y)
+  .tickSize(width)
+  .tickFormat(formatTemperature)
+  .orient("right");
 
-    var svg = d3.select("svg").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var svg = d3.select("svg").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+  svg.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
 
-    var gy = svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+  var gy = svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis);
 
-    gy.selectAll("g").filter(function(d) { return d; })
-        .classed("minor", true);
+  gy.selectAll("g").filter(function(d) { return d; })
+  .classed("minor", true);
 
-    gy.selectAll("text")
-        .attr("x", 4)
-        .attr("dy", -4);
+  gy.selectAll("text")
+  .attr("x", 4)
+  .attr("dy", -4);
 
-function formatTemperature(d) {
-  var s = formatNumber(d / 1e6);
-  return d === y.domain()[1]
-      ?  s + " degrees"
-      : s;
-}
+  function formatTemperature(d) {
+    var s = formatNumber(d / 1e6);
+    return d === y.domain()[1]
+    ?  s + " degrees"
+    : s;
+  }
 } // end of the function make_axes
+
+
